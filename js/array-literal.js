fe11524d3707b869b9b08bd81e66d686edac67c2
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-01-31 13:51:41 +0000

    added arrays js

---
+++ js/array-literal.js
@@ -0,0 +1,7 @@
+//create array and assign values
+var colors;
+colors = ['White','Black','Red'];
+
+//show first item from array
+var el = document.getElementById('colors');
+el.textContent = colors[0];
