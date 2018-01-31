6aeb7b4fc1eb484d8972cf102d9a66eae6ba17b3
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-01-31 13:35:45 +0000

    Ver2
    Concatenated name and message

--- js/string-variable.js
+++ js/string-variable.js
@@ -1,13 +1,17 @@
+// create vars
 var username;
 var message;
 
+//assign values
 username = 'Conor';
-message = 'See our upcoming range';
+message = 'Hey ' +username+ ', See our upcoming range';
 
+//get element with id of name
 var elName = document.getElementById('name');
-
+//replace content of this element
 elName.textContent = username;
 
+//get element with id of note
 var elNote = document.getElementById('note');
-
+// replace content of this element
 elNote.textContent = message;
