512e20c07f08323c0624c3d06cb59bcef6fcc5c8
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-01-31 13:55:40 +0000

    Array Update
    replaced a value

--- js/array-literal.js
+++ js/array-literal.js
@@ -2,6 +2,8 @@
 var colors;
 colors = ['White','Black','Red'];
 
+colors[2] = 'Beige';
+
 //show first item from array
 var el = document.getElementById('colors');
-el.textContent = colors[0];
+el.textContent = colors[2];
