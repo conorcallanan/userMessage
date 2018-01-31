618d0552532f283e2100b92f42c9308cfbfbcf94
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-01-31 13:30:21 +0000

    Ver1

---
+++ js/string-variable.js
@@ -0,0 +1,13 @@
+var username;
+var message;
+
+username = 'Conor';
+message = 'See our upcoming range';
+
+var elName = document.getElementById('name');
+
+elName.textContent = username;
+
+var elNote = document.getElementById('note');
+
+elNote.textContent = message;
