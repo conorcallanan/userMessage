520d55b99fa661d0f3b80506abeefe93fb046ba5
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-01-31 13:59:52 +0000

    Array Constructor
    added value using .push

--- js/array-literal.js
+++ js/array-literal.js
@@ -1,9 +1,12 @@
 //create array and assign values
-var colors;
-colors = ['White','Black','Red'];
+var colors = new Array('white',
+					   'black',
+					   'red');
+					   
+colors.push('purple');
 
 colors[2] = 'Beige';
 
 //show first item from array
 var el = document.getElementById('colors');
-el.textContent = colors[2];
+el.textContent = colors[3];
